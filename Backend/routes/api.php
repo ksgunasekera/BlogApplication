a5289d0821e70b614dc35c0efffac0690e015b78
gunasekera <ksgunasekera@gmail.com>
2018-09-26 15:23:07 +0530

    Added commenting

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -23,3 +23,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/posts','PostsController@index');
 Route::get('/search/{id}','PostsController@show');
 Route::post('/createPost','PostsController@store');
+Route::post('/createComment','CommentController@store');
--- Frontend/src/components/posts.js
+++ Frontend/src/components/posts.js
@@ -12,12 +12,32 @@ class Posts extends Component {
         super(props);
         this.state={
             posts:[],
-            po:[
-                {t1:1},
-                {t1:2},
-                {t1:45}
-            ]
+            postId:"",
+            username:"",
+            comment:"",
+            comments:[]
         }
+    }
+    handlePostId(postId) {
+        this.setState({postId});
+    }
+
+    handleUsername(username) {
+        this.setState({username});
+    }
+
+    handleComment(comment) {
+        this.setState({comment});
+    }
+
+    onSubmitComment(event) {
+        event.preventDefault();
+        let newComment= {
+            postId:this.state.postId,
+            username:this.state.username,
+            comment:this.state.comment
+        }
+        console.log(newComment);
     }
 
     componentDidMount() {
@@ -26,6 +46,10 @@ class Posts extends Component {
            console.log("Ammo");
            console.log(this.state.posts);
         });
+    }
+
+    deletePost(id) {
+
     }
 
     render() {
@@ -40,7 +64,22 @@ class Posts extends Component {
                             <p>{post.body}</p>
                             <Link to={`/editPost/${post.id}`}>Edit</Link><br />
                             <Link to={`/deletePost/${post.id}`}>Delete</Link>
+                            
                         </div>
+                        <div className="col col-md-4">
+                                <form onSubmit={this.onSubmitComment.bind(this)}>
+                                <div className="form-group">
+                                    <input type="text" className="form-control" onChange={this.handleTitle(this.state.posts[i].id)} value={this.state.posts[i].title}/>
+                                    </div>
+                                    <div className="form-group">
+                                    <input type="text" className="form-control" onChange={(event) => this.handleUsername(event.target.value)} placeholder="username"/>
+                                    </div>
+                                    <div className="form-group">
+                                    <textarea rows="2" cols="20" onChange={(event) => this.handleComment(event.target.value)}/>
+                                    </div>
+                                    <input type="submit" value="Comment" className="btn btn-primary"/>
+                                </form>
+                            </div>
                              
                      </li>
                     )
